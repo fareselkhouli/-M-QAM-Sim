d3ef887f0f5e85528474e1be0230eaa47d9ac21e
fareselkhouli <fareselkhouli@gmail.com>
2020-05-01 23:00:59 -0400

    Create mqam.m

---
+++ mqam.m
@@ -0,0 +1,22 @@
+clc
+close all
+%%initialize vars
+SNR = 20;
+M1 = 4;
+M2 = 16;
+M3 = 64;
+k1 = log2(M1); %number of bits per symbol
+k2 = log2(M2);
+k3 = log2(M3);
+
+%%read image
+image = imread('student.jpg');
+image = reshape(image,numel(image),1);
+binImage = de2bi(image);
+bitStream = reshape(binImage',numel(binImage),1);
+
+%%modulate
+modStream1 = qammod(bitStream,M1); %gray code modulated data with M1
+modStream2 = qammod(bitStream,M2);
+modStream3 = qammod(bitStream,M3);
+
