02f412e6b0cc00a259d0647af6bc81ca14048bad
OussemaTrabelsi <64674291+OussemaTrabelsi@users.noreply.github.com>
2020-05-03 16:29:51 -0400

    Update Transmitter.m

--- Transmitter.m
+++ Transmitter.m
@@ -1,9 +1,9 @@
-function modstream=Transmitter(M,imagefilename)
-k = log2(M); %number of bits per symbol
+function [modstream,image_dim,image]=Transmitter(M,imagefilename)
 %%read image
 image = imread(imagefilename);
-image = reshape(image,numel(image),1);
-binImage = de2bi(image);
+image_dim = size(image);
+imageReshape = reshape(image,numel(image),1);
+binImage = de2bi(imageReshape);
 bitStream = reshape(binImage',numel(binImage),1);
-%%modulate
-modstream=qammod(bitStream,M); %gray code modulated data with M
+%modulate
+modstream=qammod(bitStream,M,'InputType','bit');; %gray code modulated data with M
