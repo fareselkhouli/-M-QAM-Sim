0b6079cd3c27894b73f63a6c400bede95bd5ffb3
OussemaTrabelsi <64674291+OussemaTrabelsi@users.noreply.github.com>
2020-05-03 16:29:26 -0400

    Update Receiver.m

--- Receiver.m
+++ Receiver.m
@@ -1,21 +1,7 @@
-function rec_image = Receiver(M,imageFileName, dataMod)
-
-image = imread(imageFileName);
-image_dim = size(image);
-elements_image = numel(image);
-
-dataDeMod = qamdemod(dataMod,M,'gray','OutputType','bit');
-len = length(dataDeMod);
-stream8bit = uint8(dataDeMod);
-output_16qam = stream8bit(1:len);
-b2 = reshape(output_16qam,8,elements_image)';
-dec_16qam =bi2de(b2,'left-msb');
-rec_image = reshape(dec_16qam(1:numel(image)),image_dim);
-
-subplot(1,2,1);
-imshow(image);
-title('Original Image');
-
-subplot(1,2,2);
-imshow(rec_image);
-title('Received Image')
+function[rec_im,dataDeMod]=Receiver(y,M,image_dim)
+%%demodulator
+dataDeMod = qamdemod(y,M,'gray','OutputType','bit');
+%%reshape image data
+rec_im = reshape(dataDeMod,8,numel(dataDeMod)/8);
+rec_im = uint8(bi2de(rec_im'));
+rec_im = reshape(rec_im,image_dim);
