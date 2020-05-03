aacb79ef5ccf79cfca07abe7cfc76fb7ad2dcdef
Victor Abril <60946378+vasabril@users.noreply.github.com>
2020-05-03 14:55:16 -0400

    Add files via upload

--- Receiver.m
+++ Receiver.m
@@ -1,18 +1,21 @@
 function rec_image = Receiver(M,imageFileName, dataMod)
 
 image = imread(imageFileName);
-Image_dim = size(image);
+image_dim = size(image);
+elements_image = numel(image);
 
 dataDeMod = qamdemod(dataMod,M,'gray','OutputType','bit');
-
-Stream8Bits = reshape (dataDeMod,numel(image),8); 
-DecImage = bi2de(Stream8Bits);
-rec_image = reshape(DecImage,Image_dim);
+len = length(dataDeMod);
+stream8bit = uint8(dataDeMod);
+output_16qam = stream8bit(1:len);
+b2 = reshape(output_16qam,8,elements_image)';
+dec_16qam =bi2de(b2,'left-msb');
+rec_image = reshape(dec_16qam(1:numel(image)),image_dim);
 
 subplot(1,2,1);
 imshow(image);
 title('Original Image');
 
 subplot(1,2,2);
-imshow(rec_image); 
+imshow(rec_image);
 title('Received Image')
